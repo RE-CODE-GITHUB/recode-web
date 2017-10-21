63ce4812ad68d547c6e1e7bb1bb6ef3518f5c64b
bokotomo <bokotomo@me.com>
2017-10-22 03:37:23 +0900

    ルーティングの機能

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <?php
   require 'vendor/autoload.php';
   $router = new AltoRouter();
-  $router->setBasePath('/');
-  $router->map('GET', '/', './page/home');
+  $router->map( 'GET', '/', function() {
+      require __DIR__ . '/page/home/index.html';
+  });
   $match = $router->match();
+  if( $match && is_callable( $match['target'] ) ) {
+      call_user_func_array( $match['target'], $match['params'] ); 
+  } else {
+      header( $_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found');
+  }
 
